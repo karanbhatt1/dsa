22defa81dd4a05d671ba7b65e3b9a80484e97ec4
karan bhat <119851759+karanbhatt1@users.noreply.github.com>
2023-09-02 10:03:32 +0530

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+struct stack{
+    int size;
+    int* s;
+    int top;
+}st;
+void push(stack *st,int x)
+{
+    if(st->top==st->size-1)
+    {
+        cout<<"stack overflow";
+    }
+    else
+    {
+        st->top++;
+        st->s[st->top]=x;
+    }
+}
+int main() {
+    st.top=-1;
+    cout<<"Enter the size of stack:";
+    cin>>st.size;
+    st.s = new int[st.size-1];
+    cout<<"Enter the elements of stack:\n";
+    for(int i=0;i<=st.size;i++)
+    {
+    	cin>>i;
+        push(&st,i);
+    }
+    cout<<"The elements in the stack are :\n";
+    for(int j=st.top;j>=0;j--)
+    {
+    	cout<<st.s[j]<<"\n";
+	}
+    return 0;
+}
